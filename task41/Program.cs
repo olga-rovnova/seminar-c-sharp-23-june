96c0b22ddf8992a189c2ecd8ff874918983b6cdc
Olga <olga-rovnova@yandex.ru>
2022-06-25 23:56:44 +0300

    task41 file

---
+++ task41/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры числа через запятую. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("введите целые числа через запятую без пробелов: ");
+string numbers = $"{Console.ReadLine()} ";
+
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] == ',')
+    {
+        int j=i+1;
+        if (numbers[j] !='0' && numbers[j] !='-')
+        count++;
+    }
+} 
+
+if (numbers[0] !='0' && numbers[0] !='-')
+{
+    count++;
+}
+
+Console.WriteLine($"Количество положительных чисел, введенных пользователем: {count}");
